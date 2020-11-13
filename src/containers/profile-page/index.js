f2ba12cbea417bae1ab7527e8d30497eb3380648
Vu Nguyen <hello.vunguyen@gmail.com>
2020-11-12 22:51:14 -0600

    adding profile page placeholder init

---
+++ src/containers/profile-page/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../../components/layout/header';
+
+const ProfilePage = () => {
+  return (
+    <>
+      <Header />
+      <p>profile placeholder</p>
+    </>
+  );
+};
+
+export default ProfilePage;
