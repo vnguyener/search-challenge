d23906c11c57c878768871c74315d8dc1358bd81
Vu Nguyen <hello.vunguyen@gmail.com>
2020-11-15 22:38:32 -0600

    styling not-found page

--- client/src/containers/not-found/index.js
+++ client/src/containers/not-found/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useDispatch } from 'react-redux';
 import { push } from 'connected-react-router';
 import MinimalButton from 'components/shared/minimal-button';
+import './styles.scss';
 
 const NotFoundPage = () => {
   const dispatch = useDispatch();
@@ -11,13 +12,21 @@ const NotFoundPage = () => {
   };
 
   return (
-    <>
-      <div>
-        <h2>404</h2>
-        <p>Oops, looks like you're lost.</p>
-        <MinimalButton onClick={goToSearchPage}>Back to Results</MinimalButton>
-      </div>
-    </>
+    <main className="not-found-container">
+      <h2>404</h2>
+      <p>Oops, looks like you're lost.</p>
+      <MinimalButton
+        onClick={goToSearchPage}
+        propStyles={{
+          textTransform: 'uppercase',
+          border: '1px solid',
+          padding: '12px',
+          borderRadius: '8px',
+        }}
+      >
+        Back to Results
+      </MinimalButton>
+    </main>
   );
 };
 
